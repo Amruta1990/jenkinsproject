d0b60cfb6e47b03382d3f5af6c68b59f69fb60d1
Amruta1990 <amrutamnikam@gmail.com>
2022-12-23 16:13:12 +0530

    first commit

---
+++ src/test/java/com/AmrutaIT/SpringJpaRepository/SpringJpaRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AmrutaIT.SpringJpaRepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
